9f36a90f724d8f03c92fbabd8af119de6ed9d7a8
Paulo L F Casaretto <pcasaretto@gmail.com>
2012-04-11 11:09:57 -0400

    ws is now a function with autocompletion

--- system/alias.bash
+++ system/alias.bash
@@ -1,2 +1,13 @@
 alias be='bundle exec'
-alias ws='cd $WORKSPACE'
+# alias ws='cd $WORKSPACE'
+
+#ws will autocomplete with the folders in your workspace
+_ws () { 
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  local dires=$(ls $WORKSPACE)
+  COMPREPLY=( $(compgen -W "$dires" -- $cur) )
+}
+ws () { 
+  cd "$WORKSPACE/$@"
+}
+complete -F _ws ws
