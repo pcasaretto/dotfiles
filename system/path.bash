6a8d00321612907b44f7ee61abf8c270341be86f
Paulo L F Casaretto <pcasaretto@gmail.com>
2012-06-13 19:39:12 -0300

    Im now using https://github.com/carsomyr/rbenv-bundler

--- system/path.bash
+++ system/path.bash
@@ -1,3 +1,3 @@
-export PATH=".git/safe/../../bin:/usr/local/bin:$DOTFILES/bin:$PATH"
+export PATH="/usr/local/bin:$DOTFILES/bin:$PATH"
 
 #.git/safe? https://twitter.com/#!/tpope/statuses/165631968996900865
