a8ad10fc25780166909c72287d9dc79c3c9ed69a
Paulo Luis Franchini Casaretto <pcasaretto@gmail.com>
2012-09-05 12:18:10 -0300

    Add some colors to PS1

--- bash/config.bash
+++ bash/config.bash
@@ -1,4 +1,8 @@
-PS1='\w$(__git_ps1 "(%s)") \$ '
+txtgrn='\e[0;32m' # Green
+txtcyn='\e[0;36m' # Cyan
+Color_Off='\e[0m' # Text Reset
+
+PS1="\[$txtcyn\]\w\[$txtgrn\]\$(__git_ps1 '(%s)') \$ \[$Color_Off\]"
 
 export HISTCONTROL=erasedups
 export HISTSIZE=10000
