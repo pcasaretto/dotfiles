8102ec88ad563079efef5de0ef9ffd1acee2e3c7
Paulo L F Casaretto <pcasaretto@gmail.com>
2014-02-27 18:15:48 -0300

    Quit pretending like I haven't been using Atom for over a day

--- system/env.zsh
+++ system/env.zsh
@@ -1,9 +1,4 @@
-# Only set this if we haven't set $EDITOR up somewhere else previously.
-if [ -z "$EDITOR" ]; then
-  export EDITOR='mate -w'
-  # export EDITOR='subl'
-fi
+export EDITOR='atom'
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-
