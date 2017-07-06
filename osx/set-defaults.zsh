998e4eff6b866cd47a9ff884fc2dcb3a48134cc2
Paulo Casaretto <pcasaretto@gmail.com>
2017-07-06 14:18:38 -0300

    Change OSX defaults

--- osx/set-defaults.zsh
+++ osx/set-defaults.zsh
@@ -8,15 +8,12 @@
 # Run ./set-defaults.sh and you'll be good to go.
 if [ `uname -s` = "Darwin" ]
 then
-	# Disable press-and-hold for keys in favor of key repeat
-	defaults write -g ApplePressAndHoldEnabled -bool false
-
-	# Use AirDrop over every interface. srsly this should be a default.
-	defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
-
 	# Always open everything in Finder's list view. This is important.
 	defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
 	# Show the ~/Library folder
 	chflags nohidden ~/Library
+
+	# disable the horrible side swipe history control on Chrome
+	defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
 fi
