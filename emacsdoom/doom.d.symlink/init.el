833a1ad79e09d117f2f4210489a4e860eb193d6a
Paulo Casaretto <pcasaretto@gmail.com>
2022-01-26 09:08:13 -0300

    Add solidity to emacsdoom

--- emacsdoom/doom.d.symlink/init.el
+++ emacsdoom/doom.d.symlink/init.el
@@ -168,9 +168,9 @@
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
-       sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
-       ;;solidity          ; do you need a blockchain? No.
+       (sh +zsh)        ; she sells (ba|z|fi)sh shells on the C xor
+       solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        yaml
